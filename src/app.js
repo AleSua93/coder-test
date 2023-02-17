eac8f5a8db37f9c750473eb29c73d3e9a5d24e27
Alejandro Suarez <alejandronicolassuarez@hotmail.com>
2023-02-17 19:44:38 -0300

    mas codigo

--- src/app.js
+++ src/app.js
@@ -2,3 +2,7 @@ import express from "express";
 import ProductManager from "./ProductManager.js";
 
 const app = express();
+
+app.listen(8080, () => {
+  console.log("Servidor http escuchando en el puerto 8080");
+});
